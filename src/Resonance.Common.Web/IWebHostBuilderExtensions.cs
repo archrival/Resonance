56a0b79e2dffbc51a3e4c245f422ff5be2c7060e
Joshua Bahnsen <archrival@gmail.com>
2017-08-15 23:44:41 -0700

    Upgrade to dotnet 2.0

--- src/Resonance.Common.Web/IWebHostBuilderExtensions.cs
+++ src/Resonance.Common.Web/IWebHostBuilderExtensions.cs
@@ -1,125 +1,142 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Resonance.Common.Web
 {
-	public class IWebHostBuilderExtensions
-	{
-		public static IWebHostBuilder GetWebHostBuilder()
-		{
-			var builder = new ConfigurationBuilder()
-				.AddJsonFile("appsettings.json", true, true)
-				.AddEnvironmentVariables();
+    public class IWebHostBuilderExtensions
+    {
+        public static IWebHostBuilder GetWebHostBuilder()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
 
-			var configuration = builder.Build();
+            var configuration = builder.Build();
 
-			var threadCount = 32;
-			var noDelay = true;
-			var addServerHeader = true;
-			var httpEnabled = false;
-			var httpsEnabled = false;
-			var httpsConnectionFilterOptions = new HttpsConnectionFilterOptions();
+            var noDelay = true;
+            var threadCount = 32;
+            var allowSynchronousIO = false;
+            var addServerHeader = true;
+            var httpEnabled = false;
+            var httpAddress = IPAddress.Any;
+            var httpPort = 5000;
+            var httpsEnabled = false;
+            var httpsAddress = IPAddress.Any;
+            var httpsPort = 5001;
+            var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions();
 
-			var urls = new List<string>();
+            var appSettings = configuration.GetSection("AppSettings");
 
-			var appSettings = configuration.GetSection("AppSettings");
+            if (appSettings != null)
+            {
+                var kestrelSettings = appSettings.GetSection("KestrelSettings");
 
-			if (appSettings != null)
-			{
-				var kestrelSettings = appSettings.GetSection("KestrelSettings");
+                if (kestrelSettings != null)
+                {
+                    threadCount = kestrelSettings.GetValue("ThreadCount", Environment.ProcessorCount);
+                    noDelay = kestrelSettings.GetValue("NoDelay", true);
+                    addServerHeader = kestrelSettings.GetValue("AddServerHeader", true);
+                }
 
-				if (kestrelSettings != null)
-				{
-					threadCount = kestrelSettings.GetValue("ThreadCount", Environment.ProcessorCount);
-					noDelay = kestrelSettings.GetValue("NoDelay", true);
-					addServerHeader = kestrelSettings.GetValue("AddServerHeader", true);
-				}
+                var httpSettings = appSettings.GetSection("HttpSettings");
 
-				var httpSettings = appSettings.GetSection("HttpSettings");
+                if (httpSettings != null)
+                {
+                    var unsecuredSettings = httpSettings.GetSection("Unsecured");
 
-				if (httpSettings != null)
-				{
-					var unsecuredSettings = httpSettings.GetSection("Unsecured");
+                    if (unsecuredSettings != null)
+                    {
+                        httpEnabled = unsecuredSettings.GetValue("Enabled", false);
 
-					if (unsecuredSettings != null)
-					{
-						httpEnabled = unsecuredSettings.GetValue("Enabled", false);
+                        if (httpEnabled)
+                        {
+                            var listenerSettings = unsecuredSettings.GetSection("Listener");
 
-						if (httpEnabled)
-						{
-							GetListenerUrls(urls, unsecuredSettings, "http", 5000);
-						}
-					}
+                            if (listenerSettings != null)
+                            {
+                                var address = listenerSettings.GetValue("Address", "*");
 
-					var securedSettings = httpSettings.GetSection("Secured");
+                                if (address != "*")
+                                {
+                                    IPAddress.TryParse(address, out httpAddress);
+                                }
 
-					if (securedSettings != null)
-					{
-						httpsEnabled = securedSettings.GetValue("Enabled", false);
+                                httpPort = listenerSettings.GetValue("Port", 5000);
+                            }
+                        }
+                    }
 
-						if (httpsEnabled)
-						{
-							GetListenerUrls(urls, securedSettings, "https", 5001);
+                    var securedSettings = httpSettings.GetSection("Secured");
 
-							var certificateFile = securedSettings.GetValue("CertificateFile", string.Empty);
-							var certificatePassword = securedSettings.GetValue("CertificatePassword", string.Empty);
+                    if (securedSettings != null)
+                    {
+                        httpsEnabled = securedSettings.GetValue("Enabled", false);
 
-							if (!string.IsNullOrWhiteSpace(certificateFile) && !string.IsNullOrWhiteSpace(certificatePassword))
-							{
-								httpsConnectionFilterOptions = new HttpsConnectionFilterOptions
-								{
-									ClientCertificateMode = ClientCertificateMode.NoCertificate,
-									ServerCertificate = new X509Certificate2(certificateFile, certificatePassword),
-									SslProtocols = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12
-								};
-							}
-						}
-					}
-				}
-			}
+                        if (httpsEnabled)
+                        {
+                            var listenerSettings = securedSettings.GetSection("Listener");
 
-			var webHostBuilder = new WebHostBuilder()
-				.UseUrls(urls.ToArray())
-				.UseKestrel(options =>
-				{
-					options.ThreadCount = threadCount;
-					options.NoDelay = noDelay;
-					options.AddServerHeader = addServerHeader;
+                            if (listenerSettings != null)
+                            {
+                                var address = listenerSettings.GetValue("Address", "*");
 
-					if (httpsEnabled)
-					{
-						options.UseHttps(httpsConnectionFilterOptions);
-					}
-				})
-				.UseContentRoot(Directory.GetCurrentDirectory());
+                                if (address != "*")
+                                {
+                                    IPAddress.TryParse(address, out httpsAddress);
+                                }
 
-			return webHostBuilder;
-		}
+                                httpsPort = listenerSettings.GetValue("Port", 5001);
+                            }
 
-		private static void GetListenerUrls(List<string> urls, IConfigurationSection settings, string scheme, int defaultPort)
-		{
-			var listenerSettings = settings.GetSection("Listener");
+                            var certificateFile = securedSettings.GetValue("CertificateFile", string.Empty);
+                            var certificatePassword = securedSettings.GetValue("CertificatePassword", string.Empty);
 
-			if (listenerSettings != null)
-			{
-				var address = listenerSettings.GetValue("Address", "*");
-				var port = listenerSettings.GetValue("Port", defaultPort);
+                            httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions
+                            {
+                                ClientCertificateMode = ClientCertificateMode.NoCertificate,
+                                ServerCertificate = new X509Certificate2(certificateFile, certificatePassword),
+                                SslProtocols = SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12
+                            };
+                        }
+                    }
+                }
+            }
 
-				UriBuilder uriBuilder = new UriBuilder()
-				{
-					Scheme = scheme,
-					Host = address,
-					Port = port
-				};
+            var webHostBuilder = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.AllowSynchronousIO = allowSynchronousIO;
+                    options.ApplicationSchedulingMode = SchedulingMode.Inline;
+                    options.AddServerHeader = addServerHeader;
 
-				urls.Add(uriBuilder.ToString());
-			}
-		}
-	}
+                    if (httpEnabled)
+                    {
+                        options.Listen(httpAddress, httpPort);
+                    }
+
+                    if (httpsEnabled)
+                    {
+                        options.Listen(httpsAddress, httpsPort, listenOptions =>
+                        {
+                            listenOptions.NoDelay = noDelay;
+                            listenOptions.UseHttps(httpsConnectionAdapterOptions);
+                        });
+                    }
+                })
+                .UseLibuv(options =>
+                {
+                    options.ThreadCount = threadCount;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory());
+
+            return webHostBuilder;
+        }
+    }
 }
